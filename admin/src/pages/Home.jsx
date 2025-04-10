137ed9c312fc1cb728ead2bd671f46fcdf7fd413
Nithin0901 <nithinnivin76@gmail.com>
2025-04-11 00:05:10 +0530

    first committ

---
+++ admin/src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import PulseCashHomepage from './UserDetail';
+import LoanProcess from '../components/LoanProcess';
+
+export default function Home() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  
+  return (
+    <div className="flex flex-col min-h-screen">
+    
+
+      {/* Hero Section */}
+      <div className="relative flex-grow">
+        {/* Background Image and Overlay */}
+        <div className="absolute inset-0 bg-gray-200">
+          <img 
+            src="/api/placeholder/1600/900" 
+            alt="Happy couple" 
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute inset-0 bg-black opacity-40"></div>
+        </div>
+
+        {/* Hero Content */}
+        <div className="relative h-full py-16 md:py-24 px-4 md:px-8">
+          <div className="container mx-auto">
+            <div className="md:max-w-2xl lg:max-w-3xl">
+              <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-4 leading-tight">
+              Welcome to Pulsecash – Your Trusted Lending Partner
+              </h1>
+              <p className="text-white text-base sm:text-lg md:text-xl mb-8 opacity-90">
+                We will match you with a loan program that meet your financial need. In short term liquidity, by striving to make funds available to them within 24 hours of application.
+              </p>
+              <button className="bg-green-700 text-white font-medium px-6 py-3 rounded hover:bg-green-800 transition duration-300">
+              <Link to="/form" className="">
+           Apply
+          </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <PulseCashHomepage/>
+      {/* <LoanProcess/> */}
+    </div>
+  );
+}
